edff82f42a4f7b3d90e17f0d0da3f9505319858a
BeornPL <37457935+BeornPL@users.noreply.github.com>
2019-08-29 10:15:24 +0200

    1.2.1

--- src/main/java/index/TextSplitter.java
+++ src/main/java/index/TextSplitter.java
@@ -7,11 +7,11 @@ import java.util.List;
 public class TextSplitter {
 
     public static List<String> split(String text) {
-        String[] wordsArray = convertToLoweCaseLettersOnly(text).split(" ");
+        String[] wordsArray = convertToLowerCaseLettersOnly(text).split(" ");
         return new ArrayList<>(Arrays.asList(wordsArray));
     }
 
-    private static String convertToLoweCaseLettersOnly(String text) {
+    private static String convertToLowerCaseLettersOnly(String text) {
         return text.toLowerCase().replaceAll("[^\\w\\s]","");
     }
 }
